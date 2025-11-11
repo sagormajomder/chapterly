6fc791df1355c20401fe294822266ffac93ad1e2
Sagor Majomder <sagormajomder@gmail.com>
2025-11-11 20:47:22 +0600

    Add 404 error page with image and navigation link

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router';
+import ErrorImage from '../assets/404.svg';
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <section className='min-h-[80dvh] flex flex-col gap-8 items-center justify-center h-full px-4'>
+      <img className='max-w-70' src={ErrorImage} alt='Error 404 image' />
+      <div className='text-center space-y-4'>
+        <h2 className='heading-secondary'>Error 404: Page not found!</h2>
+        <p className='text-xl'>
+          We've looked everywhere but couldn't find the page you are looking
+          for.
+        </p>
+      </div>
+      <Link to='/' className='btn btn-primary'>
+        Go to Home Page
+      </Link>
+    </section>
+  );
 }
