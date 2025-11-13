951fd82a60f79d1a5b66a32d377c9ca61f6bc3b5
Sagor Majomder <sagormajomder@gmail.com>
2025-11-13 11:31:04 +0600

    Fetch book details and handle missing books with error page

--- src/pages/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa6';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useParams } from 'react-router';
 import Container from '../components/Container';
+import { useSecureAxios } from '../hooks/useSecureAxios';
+import ErrorPage from './ErrorPage';
 
 export default function BookDetailsPage() {
-  // const data = useLoaderData();
-  // console.log(data);
-  const { title, author, genre, rating, summary, coverImage, userEmail } =
-    useLoaderData().data;
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const books = useLoaderData().data;
+  const axiosSecure = useSecureAxios();
+
+  useEffect(
+    function () {
+      axiosSecure.get(`/book-details/${id}`).then(result => {
+        setBook(result.data);
+      });
+    },
+    [axiosSecure, id]
+  );
+
+  const isHas = books.some(book => book._id === id);
+
+  if (!isHas) return <ErrorPage />;
 
   return (
     <section className='py-14'>
@@ -14,32 +30,32 @@ export default function BookDetailsPage() {
         <div className='flex md:gap-8 gap-12  justify-center md:flex-row flex-col'>
           <figure className='relative xl:basis-[22%] basis-[35%] lg:basis-[30%]'>
             <img
-              className='object-cover h-100 md:h-auto'
-              src={coverImage}
-              alt={title}
+              className='object-cover h-100 '
+              src={book?.coverImage}
+              alt={book?.title}
             />
             <p className='text-lg bg-primary text-white  font-medium px-4 py-1 rounded-full max-w-fit mb-2 inline-flex items-center gap-2 absolute top-4 left-5'>
               <span className='text-yellow-300 -mt-0.5'>
                 <FaStar />
               </span>
-              {rating}
+              {book?.rating}
             </p>
           </figure>
           <div className='basis-1/2'>
-            <h2 className='heading-secondary'>{title}</h2>
+            <h2 className='heading-secondary'>{book?.title}</h2>
             <p className=''>
-              <strong>By (Author)</strong>: {author}
+              <strong>By (Author)</strong>: {book?.author}
             </p>
             <p className='mb-4'>
-              <strong>Added by:</strong> {userEmail}
+              <strong>Added by:</strong> {book?.userEmail}
             </p>
             <p className='text-lg mb-3'>
               <strong>Summary: </strong>
-              {summary}
+              {book?.summary}
             </p>
 
             <div className='flex gap-2'>
-              {genre.split('/').map((cat, i) => (
+              {book?.genre.split('/').map((cat, i) => (
                 <p
                   key={i}
                   className=' bg-accent px-4 py-1 rounded-full max-w-fit mb-2'>
